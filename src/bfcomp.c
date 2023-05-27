10f81ba9c3a82d249ccda24a217fba72a70deac8
Mirged <110278790+Mirged@users.noreply.github.com>
2023-05-27 21:48:38 +0300

    Update bfcomp.c

--- src/bfcomp.c
+++ src/bfcomp.c
@@ -4,13 +4,10 @@
 void generate_c_code(const char* source_code, FILE* file);
 
 void compile_brainfuck_to_c(const char* source_code, const char* executable_file, const char* output_file) {
-	
-	
-	
     FILE* file = fopen(output_file, "w");
     if (!file) {
-        printf("Error: Failed to open output file.\n");
-        return;
+        fprintf(stderr, "Error: Failed to open output file.\n");
+        exit(1);
     }
 
     fprintf(file, "#include <stdio.h>\n\n");
@@ -32,7 +29,8 @@ void compile_brainfuck_to_c(const char* source_code, const char* executable_file
     if (result == 0) {
         printf("Executable file '%s' generated.\n", executable_file);
     } else {
-        printf("Error: Compilation failed.\n");
+        fprintf(stderr, "Error: Compilation failed.\n");
+        exit(1);
     }
 }
 
@@ -71,14 +69,15 @@ void generate_c_code(const char* source_code, FILE* file) {
     }
 
     if (indent != 1) {
-        printf("Error: Mismatched brackets in Brainfuck code.\n");
+        fprintf(stderr, "Error: Mismatched brackets in Brainfuck code.\n");
+        exit(1);
     }
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) { // TODO: Add argument to compile braifuck code without saving C code
-        printf("Usage: %s <input_file> <output_file>\n", argv[0]);
-        return 1;
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
+        exit(1);
     }
 
     const char* input_file = argv[1];
@@ -86,8 +85,8 @@ int main(int argc, char* argv[]) {
 
     FILE* file = fopen(input_file, "r");
     if (!file) {
-        printf("Error: Failed to open input file.\n");
-        return 1;
+        fprintf(stderr, "Error: Failed to open input file.\n");
+        exit(1);
     }
 
     fseek(file, 0, SEEK_END);
@@ -96,24 +95,24 @@ int main(int argc, char* argv[]) {
 
     char* source_code = (char*)malloc(file_size + 1);
     if (!source_code) {
-        printf("Error: Failed to allocate memory.\n");
+        fprintf(stderr, "Error: Failed to allocate memory.\n");
         fclose(file);
-        return 1;
+        exit(1);
     }
 
     size_t bytes_read = fread(source_code, 1, file_size, file);
     if (bytes_read != file_size) {
-        printf("Error: Failed to read input file.\n");
+        fprintf(stderr, "Error: Failed to read input file.\n");
         fclose(file);
         free(source_code);
-        return 1;
+        exit(1);
     }
 
     source_code[file_size] = '\0';
     fclose(file);
-    
+
     const char* executable_file = output_file;
-    
+
     char c_file[256];
     sprintf(c_file, "%s.c", output_file);
 
